ce3ee06ee55f4f27d44d4ddc5ae86fd10fb07849
Art1us <109896229+Art1us@users.noreply.github.com>
2022-12-17 21:05:52 +0200

    created main page for each exchange
    implemented logic: choosing mode toggle
    conditional rendering of classes
    conditional rendering of test/start components

---
+++ src/components/StartMode/StartMode.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StartMode() {
+  return (
+    <div>StartMode</div>
+  )
+}
+
+export default StartMode
---
+++ src/components/TestMode/TestMode.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TestMode() {
+  return (
+    <div>TestMode</div>
+  )
+}
+
+export default TestMode
