6c9e5da450842155398bb9d49b90110d289df9b0
Alex-creatorA <vvvsasha228322@gmail.com>
2022-12-18 00:49:46 +0000

    change className Login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -5,19 +5,19 @@ export default function Login() {
   return (
     <div className="login__wrapper">
       <form class="login__form">
-        <div className="login__form__title">Sign-in</div>
-        <div class="form__row">
-          <label class="form__block">
-            <div class="form__title">E-mail</div>
-            <input type="text" class="form__input" />
+        <div className="login__form__main-title">Sign-in</div>
+        <div class="login__form__row">
+          <label class="login__form__block">
+            <div class="login__form__title">E-mail</div>
+            <input type="text" class="login__form__input" />
           </label>
-          <label class="form__block">
-            <div class="form__title">Password</div>
-            <input type="text" class="form__input" />
+          <label class="login__form__block">
+            <div class="login__form__title">Password</div>
+            <input type="text" class="login__form__input" />
           </label>
         </div>
-        <div className="btn__login btn__yellow">Sign in</div>
-        <div className="btn__login btn__grey">Create new account</div>
+        <div className="btn__login btn__login__yellow">Sign in</div>
+        <div className="btn__login btn__login__grey">Create new account</div>
       </form>
     </div>
   );
