1229070c36aadd53b2cc7764b1931dda7be990dc
Alex-creatorA <vvvsasha228322@gmail.com>
2022-12-19 00:55:10 +0000

    finish exchangeModal page

--- src/components/ExchangeCardModals/NewExchangeModal/NewExchangeModal.js
+++ src/components/ExchangeCardModals/NewExchangeModal/NewExchangeModal.js
@@ -27,39 +27,41 @@ function NewExchangeModal({ setShowNewModal }) {
         />
 
         <form class="newExchangeModal__form">
-          <label class="newExchangeModal__formBlock">
-            <div class="newExchangeModal__formTitle">Select Exchanges</div>
-            <select
-              className="newExchangeModal__formInput"
-              value={newExchange.exchangeName}
-              onChange={(e) =>
-                setNewExchange({ ...newExchange, exchangeName: e.target.value })
-              }
-            >
-              <option>---</option>
-              {exchangesList.map((item) => {
-                return <option key={item.id}>{item.name}</option>;
-              })}
-            </select>
-          </label>
           <div class="newExchangeModal__formCol">
-            <div class="newExchangeModal__formRow">
-              <label class="newExchangeModal__formBlock">
-                <div class="newExchangeModal__formTitle">Public Key</div>
-                <input
-                  type="text"
-                  class="newExchangeModal__formInput"
-                  value={newExchange.public}
-                  onChange={(e) =>
-                    setNewExchange({ ...newExchange, public: e.target.value })
-                  }
-                />
-              </label>
-              <label class="newExchangeModal__formBlock">
-                <div class="newExchangeModal__formTitle">Secret Key</div>
-                <input type="text" class="newExchangeModal__formInput" />
-              </label>
-            </div>
+            <label class="newExchangeModal__formBlock">
+              <div class="newExchangeModal__formTitle">Select Exchanges</div>
+              <select
+                className="newExchangeModal__formInput"
+                value={newExchange.exchangeName}
+                onChange={(e) =>
+                  setNewExchange({
+                    ...newExchange,
+                    exchangeName: e.target.value,
+                  })
+                }
+              >
+                <option>---</option>
+                {exchangesList.map((item) => {
+                  return <option key={item.id}>{item.name}</option>;
+                })}
+              </select>
+            </label>
+
+            <label class="newExchangeModal__formBlock">
+              <div class="newExchangeModal__formTitle">Public Key</div>
+              <input
+                type="text"
+                class="newExchangeModal__formInput"
+                value={newExchange.public}
+                onChange={(e) =>
+                  setNewExchange({ ...newExchange, public: e.target.value })
+                }
+              />
+            </label>
+            <label class="newExchangeModal__formBlock">
+              <div class="newExchangeModal__formTitle">Secret Key</div>
+              <input type="text" class="newExchangeModal__formInput" />
+            </label>
           </div>
           <button className="newExchangeModal__btn" onClick={clickHandler}>
             Save
