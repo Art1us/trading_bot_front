09865b91cfa28839089842cfe7f42994a308a194
Art1us <109896229+Art1us@users.noreply.github.com>
2022-12-20 09:30:05 +0200

    controlled inputs, submit handler

--- src/components/TestMode/TestMode.js
+++ src/components/TestMode/TestMode.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TestMode.css";
 
 function TestMode() {
+  const [date, setDate] = useState({
+    from: "",
+    to: "",
+  });
+
+  function sumbitHandler(e) {
+    e.preventDefault();
+    console.log(date)
+    setDate({
+      from: "",
+      to: "",
+    })
+  }
+
   return (
-    <div className="testMode">
+    <div className="testMode" onSubmit={sumbitHandler}>
       <div className="testMode__container">
         <div className="testMode__title">
           <h2>Select date range</h2>
@@ -12,17 +26,29 @@ function TestMode() {
           <form className="testMode__dateForm">
             <div className="testMode__dateFormInput--from">
               <label htmlFor="">From:</label>
-              <input type="date" />
+              <input
+                type="date"
+                value={date.from}
+                onChange={(e) =>
+                  setDate((prev) => ({ ...prev, from: e.target.value }))
+                }
+              />
             </div>
             <div className="testMode__dateFormInput--to">
               <label htmlFor="">To:</label>
-              <input type="date" />
+              <input
+                type="date"
+                value={date.to}
+                onChange={(e) =>
+                  setDate((prev) => ({ ...prev, to: e.target.value }))
+                }
+              />
             </div>
-            <input type="submit" className="testMode__startBtn" value="Start" />
+            <input type="submit" className="testMode__startBtn" value="Test" />
           </form>
           <p className="testMode__description">
-            *When you push Start Button, you will see how bot would act if it was
-            working during selected date period.
+            *When you push Start Button, you will see how bot would act if it
+            was working during selected date period.
           </p>
         </div>
       </div>
