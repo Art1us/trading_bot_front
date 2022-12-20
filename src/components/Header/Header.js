9e2218474265d506a914fa62dc8ffc524fc19da1
Vadim Galian <vadim.galian@gmail.com>
2022-12-20 19:45:29 +0200

    add botLogo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,18 @@ import "./Header.css";
 import { Link } from "react-router-dom";
 import { BsChatLeftDots } from "react-icons/bs";
 import { RxHamburgerMenu } from "react-icons/rx";
+import botLogo from "../../assets/pictures/logo-bot.png";
 
 function Header() {
   return (
     <header className="header">
       <div className="header__container">
-        <div className="header__burger">
-          <RxHamburgerMenu />
-        </div>
-        <Link to="/" style={{ textDecoration: "none"}}>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <div className="header__logo">
+            <img src={botLogo} alt="Logo" />
+          </div>
+        </Link>
+        <Link to="/" style={{ textDecoration: "none" }}>
           <h2>Trading Bot</h2>
         </Link>
         <div className="header__help">
