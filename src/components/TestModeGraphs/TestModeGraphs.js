7794e654e4f2b5bc3ce665982e0034d6680fe801
Art1us <109896229+Art1us@users.noreply.github.com>
2022-12-20 19:49:06 +0200

    added bot graph
    created bot graph with dummy data
    added Custom Tooltip

--- src/components/TestModeGraphs/TestModeGraphs.js
+++ src/components/TestModeGraphs/TestModeGraphs.js
@@ -1,10 +1,12 @@
 import React from "react";
 import LightweightChart from "../TestModeGraphs/LightweightChart/LightweightChart";
+import BotChart from "./BotChart/BotChart";
 
 function TestModeGraphs() {
   return (
     <>
       <LightweightChart />
+      <BotChart/>
     </>
   );
 }
