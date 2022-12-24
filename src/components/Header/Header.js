3d7b93bf755055906f69e164c524dc06c951c8d0
Vadim Galian <vadim.galian@gmail.com>
2022-12-24 23:07:38 +0200

    intermediate

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
@@ -7,11 +7,29 @@ import botLogo from "../../assets/pictures/logo-bot.png";
 import LanguageDropdown from "./LanguageDropdown/LanguageDropdown";
 
 function Header() {
-  const [displayDropdown, setDisplayDropdown] = useState(true);
+  const [displayDropdown, setDisplayDropdown] = useState(false);
 
   function languageClickHandler() {
     setDisplayDropdown((prev) => !prev);
   }
+
+  useEffect(() => {
+    console.log('effect1 fired')
+  
+  } )  //Effect fires on every render or re-render (update)
+
+  useEffect(() => {
+    console.log('effect2 fired')
+  
+  }, [])//undefined //undefined //Effect fires on first render only
+
+  useEffect(() => {
+    console.log('effect3 fired')
+  
+  }, [displayDropdown]) //false //true  // Effect fires on first render and any time a variable in dependency array changes
+
+
+  
 
   return (
     <header className="header">
@@ -25,12 +43,19 @@ function Header() {
           <h2>Trading Bot</h2>
         </Link>
         <div className="header__elements ">
-          <div className="header__elements--hover">
-            <RiGlobalLine
-              className="header__elments-item--right"
-              onClick={languageClickHandler}
-            />
-            {displayDropdown ? <LanguageDropdown/> : ""}
+          <div
+            className="header__elements--hover"
+            onClick={languageClickHandler}
+          >
+            <RiGlobalLine className="header__elments-item--right" />
+            {displayDropdown ? (
+              <LanguageDropdown
+                displayDropdown={displayDropdown}
+                setDisplayDropdown={setDisplayDropdown}
+              />
+            ) : (
+              ""
+            )}
           </div>
           <div className="header__elements--hover">
             <RxHamburgerMenu className="header__elments-item--right" />
--- src/components/Header/LanguageDropdown/LanguageDropdown.js
+++ src/components/Header/LanguageDropdown/LanguageDropdown.js
@@ -1,20 +1,48 @@
 import React from "react";
 import "./LanguageDropdown.css";
 import russianLanguage from "../../../assets/pictures/languageFlags/russian-language.png";
+import englishLanguage from "../../../assets/pictures/languageFlags/english-language.png";
 
-function LanguageDropdown() {
+function LanguageDropdown({ displayDropdown, setDisplayDropdown }) {
+
+  function leaveMouseHandler() {
+    setTimeout(() => {
+      setDisplayDropdown(false);
+    }, 1000);
+  }
+
+
   return (
-    <div className="languageDropdown">
+    <div
+      className="languageDropdown"
+      onClick={(e) => e.stopPropagation()}
+      onMouseLeave={leaveMouseHandler}
+    >
       <div className="languageDropdown__container">
         <h3 className="languageDropdown__title">Select Language</h3>
         <div className="languageDropdown__selectLanguageContainer">
-          <img
-            src={russianLanguage}
-            alt="russian"
-            className="languageDropdown__selectLanguage--russinLanguage"
-          />
-          <p className="languageDropdown__selectLanguage">Russian</p>
-          <p className="languageDropdown__selectLanguage">English</p>
+          <div
+            className="languageDropdown__englishContainer"
+            onClick={() => setDisplayDropdown(false)}
+          >
+            <img
+              className="languageDropdown__languageImg"
+              src={englishLanguage}
+              alt="English"
+            />
+            <p className="languageDropdown__selectLanguage">English</p>
+          </div>
+          <div
+            className="languageDropdown__russianContainer"
+            onClick={() => setDisplayDropdown(false)}
+          >
+            <img
+              className="languageDropdown__languageImg"
+              src={russianLanguage}
+              alt="russian"
+            />
+            <p className="languageDropdown__selectLanguage">Russian</p>
+          </div>
         </div>
       </div>
     </div>
