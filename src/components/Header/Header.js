2ee6f4960a62f27c71d94372e95ab3ee84d034e7
Vadim Galian <vadim.galian@gmail.com>
2022-12-25 00:08:52 +0200

    add languageDropdown

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
@@ -7,29 +7,13 @@ import botLogo from "../../assets/pictures/logo-bot.png";
 import LanguageDropdown from "./LanguageDropdown/LanguageDropdown";
 
 function Header() {
+  
   const [displayDropdown, setDisplayDropdown] = useState(false);
 
-  function languageClickHandler() {
+  function languageClickHandler(e) {
+    e.stopPropagation();
     setDisplayDropdown((prev) => !prev);
   }
-
-  useEffect(() => {
-    console.log('effect1 fired')
-  
-  } )  //Effect fires on every render or re-render (update)
-
-  useEffect(() => {
-    console.log('effect2 fired')
-  
-  }, [])//undefined //undefined //Effect fires on first render only
-
-  useEffect(() => {
-    console.log('effect3 fired')
-  
-  }, [displayDropdown]) //false //true  // Effect fires on first render and any time a variable in dependency array changes
-
-
-  
 
   return (
     <header className="header">
--- src/components/Header/LanguageDropdown/LanguageDropdown.js
+++ src/components/Header/LanguageDropdown/LanguageDropdown.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./LanguageDropdown.css";
 import russianLanguage from "../../../assets/pictures/languageFlags/russian-language.png";
 import englishLanguage from "../../../assets/pictures/languageFlags/english-language.png";
 
-function LanguageDropdown({ displayDropdown, setDisplayDropdown }) {
+function LanguageDropdown({ setDisplayDropdown }) {
+  
+  const languageDropdownRef = useRef();
+
+  useEffect(() => {
+    function closeDropdown(e) {
+      if (e.target !== languageDropdownRef.current) {
+        setDisplayDropdown(false);
+      }
+    }
+    document.body.addEventListener("click", closeDropdown);
+
+    return () => document.body.removeEventListener("click", closeDropdown);
+  }, []);
 
   function leaveMouseHandler() {
     setTimeout(() => {
@@ -11,9 +24,9 @@ function LanguageDropdown({ displayDropdown, setDisplayDropdown }) {
     }, 1000);
   }
 
-
   return (
     <div
+      ref={languageDropdownRef}
       className="languageDropdown"
       onClick={(e) => e.stopPropagation()}
       onMouseLeave={leaveMouseHandler}
