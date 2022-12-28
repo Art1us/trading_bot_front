17585f8cb53d6fd2d61783c3455d57cf5d3d5c4e
Alex-creatorA <vvvsasha228322@gmail.com>
2022-12-28 22:56:23 +0000

    did mini form without js

--- src/components/TestMode/TestMode.js
+++ src/components/TestMode/TestMode.js
@@ -2,19 +2,21 @@ import React, { useState } from "react";
 import TestModeGraphs from "../TestModeGraphs/TestModeGraphs";
 import TestModeForm from "../TestModeForm/TestModeForm";
 import "./TestMode.css";
+import StrategyForm from "./StrategyForm/StrateguForm";
 
 function TestMode() {
   const [showGraphs, setShowGraphs] = useState(false);
 
   return (
     <div className="testMode">
-      <div className="testMode__container">
+      <StrategyForm />
+      {/* <div className="testMode__container">
         <div className="testMode__title">
           <h2>Select date range</h2>
         </div>
         <TestModeForm setShowGraphs={setShowGraphs} />
         {showGraphs && <TestModeGraphs />}
-      </div>
+      </div> */}
     </div>
   );
 }
