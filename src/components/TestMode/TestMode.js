e7c6bb8787fcad31a7d5823311442ce6599927cd
Vadim Galian <vadim.galian@gmail.com>
2022-12-28 21:04:15 +0200

    change testMode date form
    after submited dateForm swaps to slected dates

--- src/components/TestMode/TestMode.js
+++ src/components/TestMode/TestMode.js
@@ -5,14 +5,24 @@ import "./TestMode.css";
 
 function TestMode() {
   const [showGraphs, setShowGraphs] = useState(false);
+  const [showDateForm, setShowDateForm] = useState(true);
+  const [selectedDates, setSelectedDates] = useState({});
+  console.log(selectedDates);
 
   return (
     <div className="testMode">
       <div className="testMode__container">
-        <div className="testMode__title">
-          <h2>Select date range</h2>
-        </div>
-        <TestModeForm setShowGraphs={setShowGraphs} />
+        {showDateForm ? (
+          <TestModeForm
+            setShowGraphs={setShowGraphs}
+            setShowDateForm={setShowDateForm}
+            setSelectedDates={setSelectedDates}
+          />
+        ) : (
+          <p>
+            From: {selectedDates.dateFrom} To: {selectedDates.dateTo}
+          </p>
+        )}
         {showGraphs && <TestModeGraphs />}
       </div>
     </div>
