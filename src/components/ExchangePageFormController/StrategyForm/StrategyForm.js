2c6b9c0ceeaad6486769a33d8c79240b7a7a9a51
Vadim Galian <vadim.galian@gmail.com>
2022-12-30 12:03:27 +0200

    fixbug with style of date&startegy form

--- src/components/ExchangePageFormController/StrategyForm/StrategyForm.js
+++ src/components/ExchangePageFormController/StrategyForm/StrategyForm.js
@@ -3,8 +3,7 @@ import { Context } from "../../../Context";
 import "./StrategyForm.css";
 
 function StrategyForm() {
-  const { setSelectedBotSettings, BOT_OPTIONS } =
-    useContext(Context);
+  const { setSelectedBotSettings, BOT_OPTIONS } = useContext(Context);
   const [selected, setSelected] = useState(BOT_OPTIONS.strategy[0]);
 
   const submit = () => {
@@ -12,22 +11,24 @@ function StrategyForm() {
   };
 
   return (
-    <div className="strategyForm__wrapper">
-      <h3 className="strategyForm__title">Select trading strategy</h3>
-      <select
-        className="strategyForm__select"
-        value={selected}
-        onChange={(e) => setSelected(e.target.value)}
-      >
-        {BOT_OPTIONS.strategy.map((item) => (
-          <option value={item} key={item}>
-            {item}
-          </option>
-        ))}
-      </select>
-      <button className="strategyForm__btn" type="button" onClick={submit}>
-        Submit
-      </button>
+    <div className="strategyForm">
+      <div className="strategyForm__wrapper">
+        <h3 className="strategyForm__title">Select trading strategy</h3>
+        <select
+          className="strategyForm__select"
+          value={selected}
+          onChange={(e) => setSelected(e.target.value)}
+        >
+          {BOT_OPTIONS.strategy.map((item) => (
+            <option value={item} key={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+        <button className="strategyForm__btn" type="button" onClick={submit}>
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
