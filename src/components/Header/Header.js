a0597983295288c33e18ded38a56f9136bd2e02a
Alex-creatorA <vvvsasha228322@gmail.com>
2023-01-02 20:14:07 +0000

    finish welcome page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,33 @@
-import React, { useState } from "react";
-import "./Header.css";
-import BurgerMenu from "./BurgerMenu/BurgerMenu";
-import { Link } from "react-router-dom";
-import { RxHamburgerMenu } from "react-icons/rx";
-import { RiGlobalLine } from "react-icons/ri";
-import { BsFillSunFill } from "react-icons/bs";
-import { BsFillMoonFill } from "react-icons/bs";
-import botLogo from "../../assets/pictures/logo-bot.png";
-import LanguageDropdown from "./LanguageDropdown/LanguageDropdown";
-import ReactSwitch from "react-switch";
+import React, { useState } from "react"
+import "./Header.css"
+import BurgerMenu from "./BurgerMenu/BurgerMenu"
+import { Link } from "react-router-dom"
+import { RxHamburgerMenu } from "react-icons/rx"
+import { RiGlobalLine } from "react-icons/ri"
+import { BsFillSunFill } from "react-icons/bs"
+import { BsFillMoonFill } from "react-icons/bs"
+import botLogo from "../../assets/pictures/logo-bot.png"
+import LanguageDropdown from "./LanguageDropdown/LanguageDropdown"
+import ReactSwitch from "react-switch"
 
 function Header() {
-  const [burgerMenuActive, setBurgerMenuActive] = useState(false);
-  const [displayDropdownActive, setDisplayDropdownActive] = useState(false);
-  const [theme, setTheme] = useState(false);
+  const [burgerMenuActive, setBurgerMenuActive] = useState(false)
+  const [displayDropdownActive, setDisplayDropdownActive] = useState(false)
+  const [theme, setTheme] = useState(false)
 
   function toggleTheme() {
-    setTheme((curr) => !curr);
-    console.log("he's alive!");
+    setTheme(curr => !curr)
+    console.log("he's alive!")
   }
 
   function languageClickHandler(e) {
-    e.stopPropagation();
-    setDisplayDropdownActive((prev) => !prev);
+    e.stopPropagation()
+    setDisplayDropdownActive(prev => !prev)
   }
 
   function burgerMenuClickHandler(e) {
-    e.stopPropagation();
-    setBurgerMenuActive((prev) => !prev);
+    e.stopPropagation()
+    setBurgerMenuActive(prev => !prev)
   }
 
   return (
@@ -98,7 +98,7 @@ function Header() {
         setBurgerMenuActive={setBurgerMenuActive}
       />
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -1,24 +1,57 @@
-import React from "react";
-import "./Welcome.css";
-import { Link } from "react-router-dom";
+import React from "react"
+import "./Welcome.css"
+import { Link } from "react-router-dom"
+import imgWelcome from "../../assets/pictures/imgWelcome.png"
 
 function Welcome() {
   return (
-    <div className="welcome">
-      <div className="welcome__container">
-        <Link to="/login" style={{ textDecoration: "none" }}>
-          <div className="welcome__button">
-            <h1 className="welcome__buttonText">Login</h1>
+    <div className="welcome__container">
+      <div className="welcome__wrapper ">
+        <div class="welcome__title-section">
+          <h1 class="welcome__main-title">
+            Start your awesome trading experience with us
+          </h1>
+          <h3 class="welcome__title-desc">
+            Join now and relax while our bot works
+          </h3>
+          <div className="welcome__btn">
+            <Link to="/login" style={{ textDecoration: "none" }}>
+              <button className="welcomeLogin__btnLog">Log In</button>
+            </Link>
+            <Link to="/register" style={{ textDecoration: "none" }}>
+              <button className="welcomeLogin__btnSign">Sign up</button>
+            </Link>
           </div>
-        </Link>
-        <Link to="/register" style={{ textDecoration: "none" }}>
-          <div className="welcome__button">
-            <h1 className="welcome__buttonText">Register</h1>
-          </div>
-        </Link>
+        </div>
+        <div className="welcome__img">
+          <img src={imgWelcome} alt="bot" className="welcomeImg__img" />
+        </div>
       </div>
+      <footer className="welcome__footer">
+        <div className="footerText">© 2023 TradingBot Inc</div>
+        <div className="footer__bullet"></div>
+        <Link href="" className="footer__link">
+          Developers
+        </Link>
+        <div className="footer__bullet"></div>
+        <Link to={""} className="footer__link">
+          Support
+        </Link>
+        <div className="footer__bullet"></div>
+        <Link to={""} className="footer__link">
+          Careers
+        </Link>
+        <div className="footer__bullet"></div>
+        <Link to={""} className="footer__link">
+          Terms
+        </Link>
+        <div className="footer__bullet"></div>
+        <Link to={""} className="footer__link">
+          Privacy
+        </Link>
+      </footer>
     </div>
-  );
+  )
 }
 
-export default Welcome;
+export default Welcome
