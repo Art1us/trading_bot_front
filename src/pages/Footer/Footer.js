73d8637aa9355bf2ebb0f6eb672478809b66b362
Alex-creatorA <vvvsasha228322@gmail.com>
2023-01-03 19:25:07 +0000

    add finish welcome page fix footer

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom"
 
 function Footer() {
   const footerItems = [
-    { id: 1, value: "Developers " },
-    { id: 2, value: "Support" },
-    { id: 3, value: "Careers " },
-    { id: 4, value: "Terms " },
-    { id: 5, value: "Privacy" },
+    { id: 1, value: "Developers ", link: "#" },
+    { id: 2, value: "Support", link: "#" },
+    { id: 3, value: "Careers ", link: "#" },
+    { id: 4, value: "Terms ", link: "#" },
+    { id: 5, value: "Privacy", link: "#" },
   ]
 
   return (
     <div className="footer__container">
-      <div className=" footer__item">© 2023 TradingBot Inc</div>
+      <div className=" footer__item footer__title">© 2023 TradingBot Inc</div>
       <ul className="footer__list">
         {footerItems.map(item => (
           <Link to={item.link} key={item.id} style={{ textDecoration: "none" }}>
