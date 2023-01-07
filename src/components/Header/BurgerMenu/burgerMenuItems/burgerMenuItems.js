255f36920e57413e2799c5c98e1ad3a6c14a11ee
Art1us <109896229+Art1us@users.noreply.github.com>
2023-01-07 18:11:30 +0200

    Update burgerMenuItems.js

--- src/components/Header/BurgerMenu/burgerMenuItems/burgerMenuItems.js
+++ src/components/Header/BurgerMenu/burgerMenuItems/burgerMenuItems.js
@@ -1,5 +1,5 @@
 export const burgerMenuItems = [
-  { id: 1, value: "Personal Profile", link: "/profile" },
+  { id: 1, value: "Profile", link: "/profile" },
   { id: 2, value: "Welcome", link: "/" },
   { id: 3, value: "Login", link: "/login" },
   { id: 4, value: "Main", link: "/main" },
