7a1734e9584867b97a0f67efc2e6343056daf5d7
Art1us <109896229+Art1us@users.noreply.github.com>
2023-01-08 16:22:19 +0200

    done

---
+++ src/components/Header/Greeting/Greeting.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react"
+import { RxHamburgerMenu } from "react-icons/rx"
+import { useAuth } from "../../../hooks/useAuth/useAuth"
+import { fetchUsername } from "../../../api/auth/fetchUsername"
+import { useApi } from "../../../hooks/useApi/useApi"
+
+function Greeting({ onClick }) {
+  const username = useApi(fetchUsername)
+  const { auth } = useAuth()
+  const controller = new AbortController()
+
+  useEffect(() => {
+    username.request(auth.access_token, controller)
+  }, [auth])
+
+  useEffect(() => {
+    console.log(username.response?.data?.data?.email)
+  }, [username.response])
+
+  return (
+    <>
+      <p className="header__userName" onClick={onClick}>
+        Hello {username.response?.data?.data?.email || "username"}
+      </p>
+      <div className="header__elements--hover" onClick={onClick}>
+        <RxHamburgerMenu className="header__elments-item--right" />
+      </div>
+    </>
+  )
+}
+
+export default Greeting
