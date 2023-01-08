09bccb3470d4fa3b553259c4007aeaffa3855996
Vadim Galian <vadim.galian@gmail.com>
2023-01-08 16:35:58 +0200

    add new buttons when you liggedIn

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -1,10 +1,12 @@
 import React from "react"
 import "./Welcome.css"
 import { Link } from "react-router-dom"
+import { useAuth } from "../../hooks/useAuth/useAuth"
 import imgWelcome from "../../assets/pictures/imgWelcome.png"
 import Footer from "../Footer/Footer"
 
 function Welcome() {
+  const { auth } = useAuth()
   return (
     <div className="welcome">
       <div className="welcome__container">
@@ -16,14 +18,29 @@ function Welcome() {
             <h3 className="welcome__title-desc">
               Join now and relax while our bot works
             </h3>
-            <div className="welcome__btn">
-              <Link to="/login" style={{ textDecoration: "none" }}>
-                <button className="welcomeLogin__btnLog">Log In</button>
-              </Link>
-              <Link to="/register" style={{ textDecoration: "none" }}>
-                <button className="welcomeLogin__btnSign">Sign up</button>
-              </Link>
-            </div>
+            {auth?.access_token ? (
+              <div className="welcome__btn">
+                <Link to="/profile" style={{ textDecoration: "none" }}>
+                  <button className="welcomeLogin__btnProfile">
+                    My Profile
+                  </button>
+                </Link>
+                <Link to="/main" style={{ textDecoration: "none" }}>
+                  <button className="welcomeLogin__btnNewBot">
+                    Create new bot
+                  </button>
+                </Link>
+              </div>
+            ) : (
+              <div className="welcome__btn">
+                <Link to="/login" style={{ textDecoration: "none" }}>
+                  <button className="welcomeLogin__btnLog">Log In</button>
+                </Link>
+                <Link to="/register" style={{ textDecoration: "none" }}>
+                  <button className="welcomeLogin__btnSign">Sign up</button>
+                </Link>
+              </div>
+            )}
           </div>
           <div className="welcome__img">
             <img src={imgWelcome} alt="bot" className="welcomeImg__img" />
