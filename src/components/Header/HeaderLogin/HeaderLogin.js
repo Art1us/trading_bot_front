bb7554a0e74293b125cbef60c2145ea87f290dc0
Vadim Galian <vadim.galian@gmail.com>
2023-01-08 15:47:18 +0200

    header auth button is working correctly

--- src/components/Header/HeaderLogin/HeaderLogin.js
+++ src/components/Header/HeaderLogin/HeaderLogin.js
@@ -1,15 +1,16 @@
+import React from "react"
+import { Link } from "react-router-dom"
 import "./HeaderLogin.css"
 
-function HeaderLogin({ setIsLoggedIn }) {
+function HeaderLogin() {
   return (
     <div className="headerLogin__wrapper">
-      <button
-        className="headerLogin__btnLog"
-        onClick={() => setIsLoggedIn(true)}
-      >
-        Log In
-      </button>
-      <button className="headerLogin__btnSign">Sign up</button>
+      <Link to="/login" style={{ textDecoration: "none" }}>
+        <button className="headerLogin__btnLog">Log In</button>
+      </Link>
+      <Link to="/register" style={{ textDecoration: "none" }}>
+        <button className="headerLogin__btnSign">Sign up</button>
+      </Link>
     </div>
   )
 }
