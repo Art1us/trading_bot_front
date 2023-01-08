c02950f5d7747ebbe57013d75e10bc9d0aed2912
Vadim Galian <vadim.galian@gmail.com>
2023-01-08 14:09:36 +0200

    create new component LoggedIn

--- src/components/LoggedIn/LoggedIn.js
+++ src/components/LoggedIn/LoggedIn.js
@@ -1,19 +1,31 @@
 import React from "react"
+import { useAuth } from "../../hooks/useAuth/useAuth"
+import { Link, useNavigate } from "react-router-dom"
 import "./LoggedIn.css"
 
 function LoggedIn() {
+  const { logout } = useAuth()
+  const navigate = useNavigate()
+
+  function logOutClickHandler() {
+    logout()
+    navigate("/")
+  }
+
   return (
     <div className="loggedIn">
       <div className="loggedIn__container">
-        <h2 className="loggedIn__title">
-          Hey, username, you are already logged in!
-        </h2>
+        <h2 className="loggedIn__title">Hey, you are already logged in!</h2>
         <p className="loggedIn__description">
           Go to your profile and start trading!🤑
         </p>
         <div className="loggedIn__btnContainer">
-          <button className="loggedIn__profileBtn">Go to your profile</button>
-          <button className="loggedIn__logOutBtn">Log Out</button>
+          <Link to="/profile" style={{ textDecoration: "none" }}>
+            <button className="loggedIn__profileBtn">Go to your profile</button>
+          </Link>
+          <button className="loggedIn__logOutBtn" onClick={logOutClickHandler}>
+            Log Out
+          </button>
         </div>
       </div>
     </div>
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,13 +3,14 @@ import "./Login.css"
 import { useNavigate, Link } from "react-router-dom"
 import useForm from "../../hooks/useForm/useForm"
 import formInputsData from "./formInputsData/formInputsData"
+import LoggedIn from "../../components/LoggedIn/LoggedIn"
 
 import { useApi } from "../../hooks/useApi/useApi"
 import { fetchLogin } from "../../api/auth/fetchLogin"
 import { useAuth } from "../../hooks/useAuth/useAuth"
 
 function Login() {
-  const { setAuth } = useAuth()
+  const { auth, setAuth } = useAuth()
   const login = useApi(fetchLogin)
   const controller = new AbortController()
   const navigate = useNavigate()
@@ -49,20 +50,26 @@ function Login() {
   }
 
   return (
-    <div className="login">
-      <div className="login__container">
-        <form className="login__form" onSubmit={submitHandler}>
-          <div className="login__formTitle">Sign-in</div>
-          <div>{inputComponents}</div>
-          <button className="login__signInBtn">Sign in</button>
-          <Link to="/register" style={{ textDecoration: "none" }}>
-            <button type="button" className="login__createAccBtn">
-              Create a new account
-            </button>
-          </Link>
-        </form>
-      </div>
-    </div>
+    <>
+      {auth?.access_token ? (
+        <LoggedIn />
+      ) : (
+        <div className="login">
+          <div className="login__container">
+            <form className="login__form" onSubmit={submitHandler}>
+              <div className="login__formTitle">Sign-in</div>
+              <div>{inputComponents}</div>
+              <button className="login__signInBtn">Sign in</button>
+              <Link to="/register" style={{ textDecoration: "none" }}>
+                <button type="button" className="login__createAccBtn">
+                  Create a new account
+                </button>
+              </Link>
+            </form>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
