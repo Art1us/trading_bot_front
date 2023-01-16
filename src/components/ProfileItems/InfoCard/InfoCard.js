d6d7a9e698684a5bfcaa006c506e9a04571ed1a7
Vadim Galian <vadim.galian@gmail.com>
2023-01-16 21:00:34 +0200

    intermediate

---
+++ src/components/ProfileItems/InfoCard/InfoCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./InfoCard.css"
+import ProfileHeader from "../ProfileHeader/ProfileHeader"
+
+function InfoCard() {
+  return <div>InfoCard</div>
+}
+
+export default InfoCard
