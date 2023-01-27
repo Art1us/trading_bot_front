0fe7fe79573068afe16b49d697def3378fcea946
Art1us <109896229+Art1us@users.noreply.github.com>
2023-01-27 13:59:40 +0200

    intermediate

--- src/helpers/LangMenuAnimation/LangMenuAnimation.js
+++ src/helpers/LangMenuAnimation/LangMenuAnimation.js
@@ -1,21 +1,16 @@
-import React, { useEffect, useState, useRef } from "react"
+import React, { useEffect, useState } from "react"
 import { CSSTransition } from "react-transition-group"
-import { ANIMATION_TIME } from "./const"
 import animationStyles from "./animation.module.css"
-import { useMount } from "hooks/useMount/useMount"
 
 const langMenuAnimation = {
   enter: animationStyles.langMenuAnimationEnter,
   enterActive: animationStyles.langMenuAnimationEnterActive,
-  /* enterDone: animationStyles.langMenuAnimationEnterDone, */
   exit: animationStyles.langMenuAnimationExit,
   exitActive: animationStyles.langMenuAnimationExitActive,
-  /* exitDone: animationStyles.langMenuAnimationExitDone, */
 }
 
 export function LangMenuAnimation({ children, opened }) {
   const [animationIn, setAnimationIn] = useState(false)
-  const ref = useRef()
 
   useEffect(() => {
     setAnimationIn(opened)
@@ -24,16 +19,12 @@ export function LangMenuAnimation({ children, opened }) {
   return (
     <CSSTransition
       in={animationIn}
-      timeout={{
-        enter: 300,
-        exit: 300,
-      }}
+      timeout={300}
       classNames={langMenuAnimation}
       mountOnEnter
       unmountOnExit
-      nodeRef={ref}
     >
-      <div ref={ref}>{children}</div>
+      <div>{children}</div>
     </CSSTransition>
   )
 }
